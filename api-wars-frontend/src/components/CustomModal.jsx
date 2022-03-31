6e1e6022441bf8ebcdb1bb1a5453ee4fc41c108c
harikae86 <harmath.esztike@gmail.com>
2022-03-31 14:00:32 +0200

    add design to modal

--- api-wars-frontend/src/components/CustomModal.jsx
+++ api-wars-frontend/src/components/CustomModal.jsx
@@ -6,23 +6,41 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import { Typography } from "@mui/material";
 import Table from "@mui/material/Table";
+import Grid from "@mui/material/Grid";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableContainer from "@mui/material/TableContainer";
 import TableRow from "@mui/material/TableRow";
-import { withStyles } from "@material-ui/core/styles";
+import { withStyles, useTheme, makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  navlinks: {
+    marginLeft: theme.spacing(5),
+    display: "flex",
+  },
+  logo: {
+    flexGrow: "1",
+    cursor: "pointer",
+  },
+  link: {
+    textDecoration: "none",
+    color: "black",
+    fontSize: "20px",
+    marginLeft: theme.spacing(20),
+    "&:hover": {
+      color: theme.palette.primary.dark,
+      borderBottom: "1px solid white",
+    },
+  },
+}));
 
 const CustomModal = (props) => {
   const { planet } = props;
+  const classes = useStyles();
+
   const [open, setOpen] = useState(false);
   const [residents, setResidents] = useState([]);
-
-  const WhiteTextTable = withStyles({
-    root: {
-      color: "#FFFFFF",
-    },
-  })(Table);
 
   const fetchResidents = (planet) => {
     planet?.residents.map(async (resident) => {
@@ -40,80 +58,87 @@ const CustomModal = (props) => {
   const handleClose = () => setOpen(false);
 
   const style = {
+    display: "grid",
     position: "absolute",
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 700,
-    bgcolor: props.theme.palette.primary.dark,
-    color: props.theme.palette.primary.light,
+    width: 800,
+    maxWidth: "75%",
+    maxHeight: "90%",
+    bgcolor: "#ffab00",
+    color: "#262626",
     border: "2px solid #000",
     boxShadow: 10,
+    overflowX: "hidden",
     p: 4,
+    my: 2,
+    mx: "auto",
+    textAlign: "center",
   };
 
   return (
     <>
       {planet.residents.length > 0 ? (
-        <Button variant="contained" color="secondary" onClick={handleOpen}>
-          <Typography
-            variant="caption"
-            component="div"
-            color="primary"
-            sx={{ flexGrow: 1 }}
-          >
-            {planet.residents.length === 1
-              ? `${planet.residents.length} resident`
-              : `${planet.residents.length} residents`}
-          </Typography>
+        <Button sx={{ flexGrow: 1 }} variant="contained" onClick={handleOpen}>
+          {planet.residents.length === 1
+            ? `${planet.residents.length} resident`
+            : `${planet.residents.length} residents`}
         </Button>
       ) : (
         "No known residents"
       )}
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-        BackdropProps={{
-          style: { backgroundColor: "rgba(0,0,0,0.05)" },
-        }}
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}></Typography>
-          <TableContainer>
-            <WhiteTextTable>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Name</TableCell>
-                  <TableCell>Height</TableCell>
-                  <TableCell>Mass</TableCell>
-                  <TableCell>Hair color</TableCell>
-                  <TableCell>Skin color</TableCell>
-                  <TableCell>Eye color</TableCell>
-                  <TableCell>Birth year</TableCell>
-                  <TableCell>Gender</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {residents?.map((resident, index) => {
-                  return (
-                    <TableRow key={index}>
-                      <TableCell>{resident.name}</TableCell>
-                      <TableCell>{resident.height}</TableCell>
-                      <TableCell>{resident.mass}</TableCell>
-                      <TableCell>{resident.hair_color}</TableCell>
-                      <TableCell>{resident.skin_color}</TableCell>
-                      <TableCell>{resident.eye_color}</TableCell>
-                      <TableCell>{resident.birth_year}</TableCell>
-                      <TableCell>{resident.gender}</TableCell>
-                    </TableRow>
-                  );
-                })}
-              </TableBody>
-            </WhiteTextTable>
-          </TableContainer>
-        </Box>
+      <Modal open={open} onClose={handleClose}>
+        <Grid
+          item
+          xs={12}
+          container
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Box sx={style}>
+            <Typography
+              variant="h4"
+              id="modal-modal-description"
+              sx={{ mt: 2 }}
+            >
+              Residents on planet {planet.name}
+            </Typography>
+            <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Height</TableCell>
+                    <TableCell>Mass</TableCell>
+                    <TableCell>Hair color</TableCell>
+                    <TableCell>Skin color</TableCell>
+                    <TableCell>Eye color</TableCell>
+                    <TableCell>Birth year</TableCell>
+                    <TableCell>Gender</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {residents?.map((resident, index) => {
+                    return (
+                      <TableRow key={index}>
+                        <TableCell>{resident.name}</TableCell>
+                        <TableCell>{resident.height}</TableCell>
+                        <TableCell>{resident.mass}</TableCell>
+                        <TableCell>{resident.hair_color}</TableCell>
+                        <TableCell>{resident.skin_color}</TableCell>
+                        <TableCell>{resident.eye_color}</TableCell>
+                        <TableCell>{resident.birth_year}</TableCell>
+                        <TableCell>{resident.gender}</TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Box>
+        </Grid>
       </Modal>
     </>
   );
